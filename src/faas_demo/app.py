897d14de117a0272c2cb8deaadc097e44b68a7a3
Aditi Aropalli <aditi.i.aropalli@happiestminds.com>
2023-11-13 15:36:28 +0000

    Updated app.py

--- src/faas_demo/app.py
+++ src/faas_demo/app.py
@@ -1,11 +1,85 @@
-from typing import Annotated
+from datetime import timedelta
 
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+import os
+
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+from model import auth, models, schemas, security
+from model.db import get_db
+from model.models import User
+from prompt_engine.prompt import generate_context, qa_template
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+router = APIRouter()
 
 
-app = FastAPI()
+@router.post("/register/", response_model=schemas.UserInDBBase)
+async def register(user_in: schemas.UserIn, db: Session = Depends(get_db)):
+    db_user = auth.get_user(db, username=user_in.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    db_user = db.query(models.User).filter(models.User.email == user_in.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
 
-@app.post("/get-chat", response_model=BotResponseModel)
-async def create_item(message: MessageModel, x_token: Annotated[str, Header()]):
-    pass
-    # return item
+    hashed_password = security.get_password_hash(user_in.password)
+    db_user = models.User(
+        **user_in.dict(exclude={"password"}), hashed_password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+@router.post("/token", response_model=schemas.Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = auth.get_user(db, username=form_data.username)
+    if not user or not security.pwd_context.verify(
+        form_data.password, user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=security.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/conversation/")
+async def read_conversation(
+    query: str,
+    current_user: schemas.UserInDB = Depends(auth.get_current_user),
+    db: Session = Depends(get_db),
+):
+    db_user = db.query(User).get(current_user.id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    context = generate_context(db_user)
+
+    llm = OpenAI(
+        temperature=0,
+        openai_api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+    prompt = PromptTemplate(
+        input_variables=["context", "question"], template=qa_template
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run(context=context, question=query)
+
+    return {"response": response}
