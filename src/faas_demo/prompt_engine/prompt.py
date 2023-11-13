70484a14adf0e6a49d4960442e64a769e8450347
aditi.i.aropalli <aditi.i.aropalli@happiestminds.com>
2023-11-13 18:17:11 +0530

    added prompt.py

---
+++ src/faas_demo/prompt_engine/prompt.py
@@ -0,0 +1,23 @@
+from model.models import User
+
+
+def generate_context(user: User):
+    context = f"""
+    User Profile:
+    ID: {user.id}
+    Username: {user.username}
+    """
+    return context
+
+
+qa_template = """
+You are ChatBot, an intelligent virtual assistant dedicated to providing personalized responses.
+You always greet the user with his or her username.
+
+With a deep understanding of the users, tailor your response to the unique needs of each individual.
+You are committed to helping users.
+
+{context}
+
+User Query: {question}
+ChatBot Response:"""
